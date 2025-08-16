52a94d1bdeebc875d3d00ce665dfd91a4ac01869
Srijan Bhandari <Srijanbhandari30@gmail.com>
2025-08-16 19:21:55 +1000

    Mongodb connection and file name change

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,8 +6,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);  // Remove deprecated options
-    console.log("MongoDB connected successfully");
+  
+    await mongoose.connect(process.env.MONGO_URI,{});  // Remove deprecated options
+    console.log("MongoDB connected successfully *****************");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
